fe0e118532939055b7ad661f32aceb0ee7390549
Martynas <martynas@atomgraph.com>
2019-06-16 22:35:02 +0200

    Added double quotes on variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-mount -o size=$VARNISH_STORAGE_SIZE \
+mount -o size="$VARNISH_STORAGE_SIZE" \
   -t tmpfs \
   tmpfs /var/lib/varnish
 
-varnishd -f ${VARNISH_VCL_CONF} \
-  -F -a ${VARNISH_LISTEN_ADDRESS}:${VARNISH_LISTEN_PORT} \
-  -p thread_pool_min=${VARNISH_MIN_THREADS} \
-  -p thread_pool_max=${VARNISH_MAX_THREADS} \
-  -p thread_pool_timeout=${VARNISH_THREAD_TIMEOUT} \
-  -s ${VARNISH_STORAGE} \
-  -S ${VARNISH_SECRET_FILE} \
-  -t ${VARNISH_TTL} \
-  -T ${VARNISH_ADMIN_LISTEN_ADDRESS}:${VARNISH_ADMIN_LISTEN_PORT}
+varnishd -f "$VARNISH_VCL_CONF" \
+  -F -a "$VARNISH_LISTEN_ADDRESS":"$VARNISH_LISTEN_PORT" \
+  -p thread_pool_min="$VARNISH_MIN_THREADS" \
+  -p thread_pool_max="$VARNISH_MAX_THREADS" \
+  -p thread_pool_timeout="$VARNISH_THREAD_TIMEOUT" \
+  -s "$VARNISH_STORAGE" \
+  -S "$VARNISH_SECRET_FILE" \
+  -t "$VARNISH_TTL" \
+  -T "$VARNISH_ADMIN_LISTEN_ADDRESS":"$VARNISH_ADMIN_LISTEN_PORT"
